8dfa9154315f9eeeaecab2570c6548947dae90ab
j-elliott3 <jake.elliott3@outlook.com>
2025-12-25 12:17:44 -0500

    Open database

---
+++ internal/data/db.go
@@ -0,0 +1,20 @@
+package data
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const dbpath = "crm.db"
+
+
+func OpenDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, error
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
